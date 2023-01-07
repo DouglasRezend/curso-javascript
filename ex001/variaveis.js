56a20bcc62e1882f41f70f7886a7ec430561a1e5
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-06 21:29:05 -0300

    Update variaveis.js

--- ex001/variaveis.js
+++ ex001/variaveis.js
@@ -1,2 +1,13 @@
 //var , let , const
-//var 
+
+//var  -> ESS - Cross-browser
+//let e const -> ES2015
+
+
+let teste = "minha string";
+teste =10;
+
+const teste2 = 10
+teste2 = 10;
+
+console.log(teste2);
