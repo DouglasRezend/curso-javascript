39eedc5b5963314a4a99001b65ee8a0a23c85a50
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-09 10:38:03 -0300

    operadores de atribuicao

--- revisao-logica/operadores.js
+++ revisao-logica/operadores.js
@@ -9,9 +9,18 @@
 
 let n1 = 10;
 let n2 = 5;
+
+//operadores aritmeticos
 console.log(n1 + n2);
 console.log(n1 - n2);
 console.log(n1 * n2);
 console.log(n1 / n2);
 console.log( n1 % n2);
 console.log(2 ** 3);
+
+// operadores de atribuiçao
+
+let n3 = 25;
+//n3 = n3 + 15;
+n3 **= 2
+console.log (n3);
