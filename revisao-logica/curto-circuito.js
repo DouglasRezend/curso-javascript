185aa77067407daada3cfab0bb06b9e7d0659df3
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-10 09:50:20 -0300

    curto circuito

---
+++ revisao-logica/curto-circuito.js
@@ -0,0 +1,13 @@
+let n = 0
+
+n = n || 10
+
+console.log(n)
+
+let = isvalid = true
+
+//if (isvalid){
+//    console.log(" e valido")
+//}
+//isvalid && console.log(" e valido ")
+isvalid || console.log ("e valido")
