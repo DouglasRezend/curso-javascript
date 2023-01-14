45878c6a52a7f97cf811882cffb05ee7c2025ffb
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-14 12:52:00 -0300

    arrays

---
+++ revisao-logica/arrays.js
@@ -0,0 +1,20 @@
+const arr = new Array()
+
+const arr2 = new Array(true , "douglas" , 26 , new Array(2 , 4 , 10))
+
+arr[0] = "douglas"
+arr[1] = 40
+console.log(arr)
+console.log(arr2[3][arr2[3].length - 1])
+
+const arr3 = ["douglas" , 40 , [3,6,9] , true]
+arr3[4] = "a"
+arr3[arr3.length] ="b"
+arr3.push("push")
+//arr3.push("a", "b", "c")
+
+console.log(arr3)
+console.log(arr3[2])
+console.log(arr3[2][0])
+let n = 6
+console.log(arr3[n])
