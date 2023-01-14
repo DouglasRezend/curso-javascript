76a00d395f31402484b416968a827261151fec20
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-14 19:32:48 -0300

    objetos

---
+++ revisao-logica/objects.js
@@ -0,0 +1,23 @@
+//const nomes = ["daniel" , "maria" , "joao"]
+//const idades =[40 , 28 , 35]
+//console.log(nomes[0], idades[0])
+/*
+const pessoa = new Object()
+pessoa.nome = "daniel"
+pessoa.idade = 40
+console.log (pessoa)
+let prop = "nome"
+console.log(pessoa[prop])
+console.log(pessoa.idade)
+*/
+
+const pessoa = {
+    nome: "douglas",
+    idade: 26
+}
+pessoa.nome = "douglas"
+pessoa.idade = 26
+console.log (pessoa)
+let prop = "nome"
+console.log(pessoa[prop])
+console.log(pessoa.idade)
