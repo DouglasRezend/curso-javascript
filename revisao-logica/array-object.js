edcc67a81ad27146dff25be93c50caf232a00d82
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-16 17:53:43 -0300

    array objetos

---
+++ revisao-logica/array-object.js
@@ -0,0 +1,18 @@
+const nomes= ["daniel" , "maria" , ""]
+const pessoa = {nome: "daniel" , idade: 10 , email: ""}
+const pessoa1 = {}
+
+const pessoas = [ {
+    nome: "daniel",
+    idade: 40
+} , {
+    nome: "maria",
+    idade: 28
+} , {
+    nome: "helena",
+    idade: 45
+} , {
+    nome: "joao",
+    idade: 18
+}]
+console.log(pessoas)
