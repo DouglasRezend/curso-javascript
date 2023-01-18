3019cc876fb548503df312165cd84a9e5c53e78f
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-18 15:45:58 -0300

    .

---
+++ introduçao/primeiro-exemplo.js
@@ -0,0 +1,11 @@
+console.log("javascript funcionando 2222")
+/*
+let nome = "douglas"
+console.log(10 + 20)
+*/
+
+console.log("qualquer coisa")
+//uma unica linha
+let nome = "Douglas"
+alert(nome)
+
