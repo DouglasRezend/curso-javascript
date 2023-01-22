03a42c3bab54a6b84b44b64849260cacf323600a
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-01-22 14:12:00 -0300

    sistema lexico

---
+++ introduçao/lexico2.js
@@ -0,0 +1,19 @@
+let x = 10
+x ="uma string"
+
+
+let msg = "uma \"string"
+console.log(msg)
+
+console.log( "2" *2)
+
+function teste(){
+    console.log(this)
+}
+teste()
+
+const obj = {
+    n:0,
+    teste
+}
+obj.teste()
